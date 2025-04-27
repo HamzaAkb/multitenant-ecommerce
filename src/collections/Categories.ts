89d97003c58a9c4307034d8a014c340c3a88ce9b
HamzaAkb <hamzawakbar@gmail.com>
2025-04-27 11:05:11 +0500

    05: search filters

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -7,6 +7,30 @@ export const Categories: CollectionConfig = {
             name: "name",
             type: "text",
             required: true,
+        },
+        {
+            name: "slug",
+            type: "text",
+            required: true,
+            unique: true,
+            index: true
+        },
+        {
+            name: "color",
+            type: "text",
+        },
+        {
+            name: "parent",
+            type: "relationship",
+            relationTo: "categories" as any,
+            hasMany: false,
+        },
+        {
+            name: "subcategories",
+            type: "join",
+            collection: "categories" as any,
+            on: "parent",
+            hasMany: true,
         }
     ]
 }
